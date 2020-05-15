828f03516ac89b11cd811bf577c7eb939bcd6cfd
sneh96 <snehalpatel7095@gmail.com>
2020-05-15 01:03:05 -0400

    another commit

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, StyleSheet, Button, Image } from 'react-native';
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
+
+const GameOverScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <TitleText>The Game is Over!</TitleText>
+      <View style={styles.imageContainer}>
+        <Image 
+        style={styles.image} 
+        source={require('../assets/icon.png')}
+        resizeMode='cover' />
+      </View>
+      <BodyText>Number of rounds : {props.roundsNumber}</BodyText>
+      <BodyText>Number was : {props.userNumber}</BodyText>
+      <Button title='NEW GAME' onPress={props.onRestart} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imageContainer: {
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: 'black',
+    width: 300,
+    height: 300,
+    overflow: 'hidden',
+    marginVertical: 30
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  }
+});
+
+export default GameOverScreen;
